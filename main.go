391ee7a2a054865c4ca13f5175fb93c02037d7ca
caork <34786132+caork@users.noreply.github.com>
2022-01-17 00:32:47 +0800

    add cpu profile

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"milk-collection/LinkedRoute"
 	"milk-collection/parser"
+	"os"
+	"runtime/pprof"
 )
 
 func main() {
+	f, _ := os.OpenFile("cpu.profile", os.O_CREATE|os.O_RDWR, 0644)
+	defer f.Close()
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
 	df := parser.TableLoader("finland1000.csv")
 	// Summarize(&df, NearestNeighbor, "NearestNeighbor")
 	// Summarize(&df, NearestInsert, "NearestInsert")
